23b430ce2547b69fa35f51a6383085d2f0040596
Sam Gladstone <gladston@umich.edu>
2014-06-15 18:04:29 -0400

    DB changes to reflect GitHub users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,25 @@
 class User < ActiveRecord::Base
   has_many :help_requests
+  has_many :comments
+
+  def self.find_or_create_from_omniauth(auth)
+    provider = auth.provider
+    uid = auth.uid
+
+    find_by(provider: provider, uid: uid) ||
+      create_from_omniauth(auth)
+  end
+
+  def self.create_from_omniauth(auth)
+    create(
+      provider: auth.provider,
+      uid: auth.uid,
+      username: auth.info.nickname,
+      email: auth.info.email,
+      avatar_url: auth.info.image
+    )
+  end
+
+  has_many :comments
+  has_many :help_requests
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140614183734) do
+ActiveRecord::Schema.define(version: 20140614201302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,11 @@ ActiveRecord::Schema.define(version: 20140614183734) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "name",       null: false
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "username"
+    t.string   "email"
+    t.string   "avatar_url"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
